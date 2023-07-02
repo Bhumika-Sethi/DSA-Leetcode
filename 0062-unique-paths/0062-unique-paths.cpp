59a28591539e232aeada893b6dd7c1f64897322c
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2023-07-03 03:14:11 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.08%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int path(int i, int j, vector<vector<int>>& dp){
+        if(i==0 && j==0){
+            return 1;
+        }
+        if(i<0 || j<0){
+            return 0;
+        }
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int up = path(i-1,j,dp);
+        int down = path(i,j-1,dp);
+        
+        return dp[i][j] = up + down;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return path(n-1,m-1,dp);
+    }
+};
