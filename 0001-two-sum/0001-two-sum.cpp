3bcc8a82b7b27e1d3d974055e8b62c2d60e77dc1
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2023-07-11 10:14:35 +0530

    Time: 16 ms (62.31%), Space: 11.8 MB (11.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for(int i = 0; i<n; i++){
-            int sum = nums[i];
-            for(int j = i+1; j<n; j++){
-                sum += nums[j];
-                if(target == sum){
-                    return {i,j};
-                }
-                sum -= nums[j];
-            }
-        }
-        return {-1,-1};
-    }
+    
+     vector<int> twoSum(vector<int>& nums, int target) {
+         map<int,int> mp;
+         int n = nums.size();
+         for(int i = 0; i<n; i++){
+             if(mp[target-nums[i]]){
+                 return {mp[target-nums[i]]-1,i};
+             }
+             mp[nums[i]] = i+1;
+         }
+         return {-1,-1};
+     }
+    
+    
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int sum = 0;
+//         int i = 0;
+//         int j = n-1;
+//         sort(nums.begin(),nums.end());
+// //         two index not best method if we have to print the index as due to sorting the indexes would get shattered
+//        while(i<j){
+//            cout<<" the number is "<<nums[i]<<" i is "<<i<<" j is "<<j<<" number is "<<nums[j]<<endl;
+//            if(nums[i]+nums[j]==target){
+//                return {i,j};
+//            }
+//            else if(nums[i]+nums[j]<target){
+//                i++;
+//            }
+//            else if(nums[i]+nums[j]>target){
+//                j--;
+//            }
+//        }
+//         return {-1,-1};
+//     }
 };
