c5999975f818d86f7a03535181db94d598a5c1ad
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2023-07-14 15:52:06 +0530

    Time: 4 ms (33.88%), Space: 6.7 MB (76.34%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,42 +1,33 @@
 class MyStack {
-    queue<int> q1;
-    queue<int> q2;
+    queue<int> q;
 public:
     MyStack() {
-//        x -> q2;
-//         q1 -> q2;
-//         q2 -> q1;
-    }
-    
-    void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            int frot = q1.front();
-            q2.push(frot);
-            q1.pop();
-        }
-//         swap q1, q2 is eqivalent to transfer all the element one by one from q2 to q1
-        swap(q1,q2);
-        // while(!q2.empty()){
-        //     int fron = q2.front();
-        //     q2.pop();
-        //     q1.push(fron);
-        // }
-    }
-    
-    int pop() {
-        int ele = q1.front();
-        q1.pop();
-        return ele;
         
     }
     
+    void push(int x) {
+        q.push(x);
+        int i = 0;
+        while(i<q.size()-1){
+            int ele = q.front();
+            q.pop();
+            q.push(ele);
+            i++;
+        }
+    }
+    
+    int pop() {
+        int ele = q.front();
+        q.pop();
+        return ele;
+    }
+    
     int top() {
-        return q1.front();
+        return q.front();
     }
     
     bool empty() {
-        return q1.empty();
+        return q.empty();
     }
 };
 
