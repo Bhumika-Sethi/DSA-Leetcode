bfab54fd8b132a09950265d7e2d5f37af8104835
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-05-31 12:00:54 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.5%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    //O(N^2) - Bruteforce solution
-        for(int i = 0; i<nums.size(); i++){
-            int ele = nums[i];
-            int cnt = 0;
-            for(int j = 0; j<nums.size(); j++){
-                if(nums[j]==ele){
-                    cnt++;
-                }
-            }
-            if(cnt==1){
-                return ele;
-            }
-        }
-        return -1;
+    //Optiomal sol - TL - O(N)
+    //             - SP - O(1)
+    //XOR Operation -> number^number = 0
+    //              -> 0^number = number
+    int missingNumb = 0;
+
+    for(int i = 0; i<nums.size(); i++){
+        missingNumb = missingNumb^nums[i];
+    }
+    return missingNumb;
     }
 };
