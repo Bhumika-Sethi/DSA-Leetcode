22515d06c2dc6368c14a29f4649bead71e3f8e50
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-06-01 12:45:04 +0530

    Time: 0 ms (100%), Space: 14.8 MB (50.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    
-     vector<int> twoSum(vector<int>& nums, int target) {
-         map<int,int> mp;
-         int n = nums.size();
-         for(int i = 0; i<n; i++){
-             if(mp[target-nums[i]]){
-                 return {mp[target-nums[i]]-1,i};
-             }
-             mp[nums[i]] = i+1;
-         }
-         return {-1,-1};
-     }
-    
-    
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         int n = nums.size();
-//         int sum = 0;
-//         int i = 0;
-//         int j = n-1;
-//         sort(nums.begin(),nums.end());
-// //         two index not best method if we have to print the index as due to sorting the indexes would get shattered
-//        while(i<j){
-//            cout<<" the number is "<<nums[i]<<" i is "<<i<<" j is "<<j<<" number is "<<nums[j]<<endl;
-//            if(nums[i]+nums[j]==target){
-//                return {i,j};
-//            }
-//            else if(nums[i]+nums[j]<target){
-//                i++;
-//            }
-//            else if(nums[i]+nums[j]>target){
-//                j--;
-//            }
-//        }
-//         return {-1,-1};
-//     }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // BETTER/BEST APPROACH -> TC : O(N)
+        //             -> SC : O(N)
+        // In map we'll be storing the element and the index
+        // First check if element in map, if yes return both the indexes
+        // otherwise add the element into the map with its index
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(int i = 0; i<n; i++){
+            int targetEle = target - nums[i];
+           if(mp.find(targetEle)!=mp.end()){
+            return {i,mp[targetEle]};
+           }
+           mp[nums[i]] = i; 
+        }
+        return{-1,-1};
+    }
 };
