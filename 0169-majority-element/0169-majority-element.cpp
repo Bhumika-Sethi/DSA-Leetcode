86c0ab90e6b3eadc59b34f6da443d88c9de7218d
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-06-01 19:19:16 +0530

    Time: 0 ms (100%), Space: 28.1 MB (84.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // Optimal solution - Moore’s Voting Algorithm
+        // TC - O(N)
+        // SC - O(1)
         int n = nums.size();
-        int cnt = 0;
-        int maj_ele;
-        for(int i = 0; i<n; i++){
-            if(cnt==0){
-                maj_ele = nums[i];
+        // Take first element and set count to 1
+        int ele = nums[0];
+        int cnt = 1;
+        // now run loop from 1 to n
+        for(int i = 1; i<n; i++){
+            // If current element is equal to ele, then increment counter
+            if(ele==nums[i]){
                 cnt++;
-            }    
-            else if(maj_ele!=nums[i]){
-                cnt--;
             }
-            else{
-                cnt++;
+            // Otherwise decrement count
+            else if(ele!=nums[i]){
+                cnt--;
+                // And check if count == 0, then change the element and increment count for that ele
+                if(cnt==0){
+                    ele = nums[i];
+                    cnt++;
+                }
             }
         }
-        return maj_ele;
+        // Only that element having majority occurance will survive, rest will get cancel out
+        return ele;
     }
 };
