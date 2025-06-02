d622899f93edd4e53215143b195cf7a7aafe7cd6
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-06-02 19:03:32 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.49%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // find the break point - increasing and then decreasing is consider as break point
-        // traverse from end;
+        // Simple observation
+        // 1. Longest prefix we need to have
+                // So, that means a[i]<a[i+1] -> breakpoint
+                // Start from the end and compare when a[i]<a[i+1], that means breakpoint there
+                // So store the index we need to switch in ind
+                // If ind = -1, than that means no breakpoint. In that case just reverse
+                // reverse of current array is the ans.
+        // 2. Now find the element just greater than ind 
+                // the left of breakpoint is sorted in increasing order from back
+                // Run the loop from i = n-1 to ind
+                // swap ind element with ith element
+                // now also after breakpoint array is sorted
+        // 3.Reverse the array after breakpoint
+                // Because it is increasing from end and decreasing from start
+                // So, we need the smallest -> sort in increasing order
+                // So just reverse
+
+        // Optimal approach TC -> O(3*N)
+        //                  SC -> O(1)
         int n = nums.size();
-        int break_point = -1;
-        for(int i = n-2; i >= 0; i--){
+        int ind = -1;
+        // Find from the back the breakpoint(ind) val
+        for(int i = n-2; i>=0; i--){
             if(nums[i]<nums[i+1]){
-                break_point = i;
+                ind = i;
                 break;
             }
         }
-        
-//      If breakpoint is still -1, then this means that the array is in decreasing order, so next permultation would be increasing order array
-        if(break_point==-1){
+
+        // If still -1
+        // Just reverse and return the answer
+        if(ind==-1){
             reverse(nums.begin(),nums.end());
             return;
         }
-        
-        
-//      Again traverse from the end and find the element just greater than nums[index]
-        for(int i = n-1; i>break_point; i--){
-                if(nums[break_point]<nums[i]){
-                    swap(nums[i],nums[break_point]);
-//                     don't forget to break out of it.
-                    break;
-                }
+
+        // Otherwise from back run the loop till ind and swap with just greater element
+        for(int i = n-1; i>ind; i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                break;
+            }
         }
-         // Step 3: reverse the right half:
-        reverse(nums.begin()+break_point+1,nums.end());
-        return;
+
+        // Now to get the smallest from ind+1 to n -> Just reverse
+        reverse(nums.begin()+ind+1,nums.end());
+
     }
 };
