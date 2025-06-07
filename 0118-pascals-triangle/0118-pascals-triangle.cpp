1b1469ab678d141d83d687e3b7a8fccc179c2a26
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-06-07 16:18:06 +0530

    Time: 3 ms (8.2%), Space: 9.7 MB (32.24%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,23 +1,32 @@
 class Solution {
-    vector<int> generateRow(int row){
-        vector<int> res;
-        int ans = 1;
-        res.push_back(ans);
-        for(int i = 1; i<row; i++){
-            ans *= row-i;
-            ans /= i;
-            res.push_back(ans);
-        }
-        return res;
-    }
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
-        
-        for(int i = 1; i<=numRows; i++){
-            vector<int> temp = generateRow(i);
-            result.push_back(temp);
+    // TC for generate row = O(N)
+    vector<int> generateRow(int rowNum){
+        // Now for each row there are elements  = rowNumber
+        vector<int> ans;
+        // First number is always 1 so we'll add one and then run from 1 -> rowNum-1
+        // Now notice for each element the formula is {row-1Ccol-1}-> we have 0 based indexing
+        // -> So for element at index 1 and row 6th -> it is 5/2
+        // -> For 2nd index, 6th row -> 5*4/2*1 -> answer*(row-i) -> answer/i
+        ans.push_back(1);
+        long long answer = 1;
+        for(int i = 1; i<rowNum; i++){
+            answer *= (rowNum-i);
+            answer /= i;
+            ans.push_back(answer);
         }
-        return result;
+        return ans;
+    }
+    // Optimal Approach -> TC - O(N*N)
+    //                 -> SC - O(1)
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        // Run a loop to generate all the rows
+        // As we can notice for i = 5, we have 5 rows
+        for(int i = 1; i<=numRows; i++){
+            // Here we will pass rowNum and the function will return row elements for generated row 
+            ans.push_back(generateRow(i));
+        }
+        return ans;
     }
 };
