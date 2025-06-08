3d917284b65e7b665e16b3deae6d57ff39846eb7
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-06-08 18:24:51 +0530

    Time: 2 ms (93.3%), Space: 23.9 MB (59.21%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // Optimal - TC - O(N)
+        //         - SC - O(1)
+        sort(intervals.begin(),intervals.end());
         int n = intervals.size();
         vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
-        ans.push_back(intervals[0]);
-        for(int i = 1; i<n; i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(end <= ans.back()[1]){
-                continue;
+        // Intuition
+        // Insert the element into ans
+        // Run a loop from 0 to n
+        // Check if last inserted element's end is greater than or equal to front of interval
+        // If yes, then check whose end is greater
+        // If ans's end is greater than let it be
+        // Otherwise, change ans's end to interval's end
+        // If the ans end is smaller than interval's front or it is the 1st element then just insert it into answer
+        for(int i = 0; i<n; i++){
+            if(ans.size()!=0 && ans.back()[1]>=intervals[i][0] && ans.back()[1]<intervals[i][1]){
+                ans.back()[1] = intervals[i][1];
             }
-            else if(ans.back()[1]>=start){
-                ans.back()[1] = end;
-            }
-            else if(ans.back()[1]<start){
-                ans.push_back({start,end});
+            else if(ans.size()==0 || (ans.size()!=0 && ans.back()[1]<intervals[i][0])){
+                ans.push_back(intervals[i]);
             }
         }
         return ans;
