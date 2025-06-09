e7820b7663ca7169b8d1fa50abcce29d3a27e0ac
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-06-09 17:18:15 +0530

    Time: 0 ms (100%), Space: 12.2 MB (95.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int len = n + m;
-        int gap = (len)/2 + len%2;
-        nums1.resize(n+m);
-        int i = m;
-        for(auto it : nums2){
-            nums1[i++] = it;
+        // So, better approach is by not using any extra space
+        // TC -> O(N+M) + O(NlogN) + O(MlogM)
+        // SC -> O(1)
+        // Intuition - As both the arrays are sorted,so start from the right of 1st array and left of 2nd array
+        // All the elements smaller should be present in array 1 and greater in array 2
+        // After this, just sort both the arrays and store result in array one as it is having space for both the array elements
+        int left = m-1;
+        int right = 0;
+        // Take care of boundaries and swap till the condition of  nums1[left] > nums2[right]
+        // Once we reach nums1[left] >= nums2[right]. Stop
+        while(left>=0 && right<n && nums1[left]>nums2[right]){
+            swap(nums1[left--],nums2[right++]);
         }
-        while(gap>0){
-            int left = 0;
-            int right = left + gap;
-            
-            while(right<len){
-                if(nums1[left]>nums1[right]){
-                    swap(nums1[left],nums1[right]);
-                }
-                left++;
-                right++;
-            }
-            if(gap==1){
-                break;
-            }
-            gap = gap/2 + gap%2;
+        sort(nums1.begin(),nums1.begin()+m);
+        sort(nums2.begin(),nums2.end());
+        // To accommodate this, nums1 has a length of m + n. Store store all then n elements present in nums2 array into end of nums1
+        for(int i = m; i<nums1.size(); i++){
+                nums1[i] = nums2[i-m];
         }
     }
 };
