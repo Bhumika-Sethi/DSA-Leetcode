772cf2724090e0944fe70b5521802de621c128ac
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2025-12-20 12:17:10 +0530

    Time: 4 ms (81.78%), Space: 13.1 MB (58.29%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,56 +1,34 @@
 class Solution {
-    int rec(int i, int j, string& str1, string& str2, vector<vector<int>>& dp){
-        if(i==str1.size()){
-            return str2.size() - j; 
-        }
-        if(j==str2.size()){
-            return str1.size() - i;
-        }
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int match = 1e9;
-        int notMatch = 1e9;
-        if(str1[i]==str2[j]){
-            match = 0 + rec(i+1,j+1,str1,str2,dp);
-        }
-        else{
-            notMatch = 1 + min(rec(i+1,j+1,str1,str2,dp),min(rec(i+1,j,str1,str2,dp),rec(i,j+1,str1,str2,dp)));
-        }
-        return dp[i][j] = min(match,notMatch);
-    }
 public:
+    int minOper(int ind1, int ind2, string& word1, string& word2, vector<vector<int>>& dp){
+        if(ind2<0){
+            return ind1+1;
+        }
+        if(ind1<0){
+            return ind2+1;
+        }
+
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
+        }
+
+        // matching
+        if(word1[ind1]==word2[ind2]){
+            return dp[ind1][ind2] = minOper(ind1-1,ind2-1,word1,word2,dp);
+        }
+
+        // notMatching
+        int del = 1 + minOper(ind1-1,ind2,word1,word2,dp);
+        int ins = 1 + minOper(ind1,ind2-1,word1,word2,dp);
+        int repl = 1 + minOper(ind1-1,ind2-1,word1,word2,dp);
+
+        return dp[ind1][ind2] = min(min(del,ins),repl);
+    }
+
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        if(n==0){
-            return m;
-        }
-        if(m==0){
-            return n;
-        }
-        vector<int> curr(m+1,0), next(m+1,0);
-        
-        for(int j = 0; j<=m; j++){
-            next[j] = m - j;
-        }
-        for(int i = n-1; i>=0; i--){
-            curr[m] = n-i;
-            for(int j = m-1; j>=0; j--){
-                int match = 1e9;
-                int notMatch = 1e9;
-                   if(word1[i]==word2[j]){
-                         match = 0 + next[j+1];
-                    }
-                    else{
-                         notMatch = 1 + min(next[j+1],min(next[j],curr[j+1]));
-                    }
-                curr[j] = min(match,notMatch);
-            }
-            next = curr;
-        }
-        
-        return curr[0];
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return minOper(n-1,m-1,word1,word2,dp);
     }
 };
