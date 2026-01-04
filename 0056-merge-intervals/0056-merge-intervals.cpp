c1ae573a50a200c1fbfc90d3b18203b34b1434ee
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-01-04 22:49:44 +0530

    Time: 7 ms (42.16%), Space: 23.9 MB (33.5%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // Optimal - TC - O(N)
-        //         - SC - O(1)
-        sort(intervals.begin(),intervals.end());
         int n = intervals.size();
-        vector<vector<int>> ans;
-        // Intuition
-        // Insert the element into ans
-        // Run a loop from 0 to n
-        // Check if last inserted element's end is greater than or equal to front of interval
-        // If yes, then check whose end is greater
-        // If ans's end is greater than let it be
-        // Otherwise, change ans's end to interval's end
-        // If the ans end is smaller than interval's front or it is the 1st element then just insert it into answer
-        for(int i = 0; i<n; i++){
-            if(ans.size()!=0 && ans.back()[1]>=intervals[i][0] && ans.back()[1]<intervals[i][1]){
-                ans.back()[1] = intervals[i][1];
-            }
-            else if(ans.size()==0 || (ans.size()!=0 && ans.back()[1]<intervals[i][0])){
-                ans.push_back(intervals[i]);
-            }
+       sort(intervals.begin(), intervals.end());
+       vector<vector<int>> ans;
+       vector<int> currentInterval = intervals[0]; 
+
+       for(int i = 0; i<n; i++){
+        // Overlapping - if start time is falling under curent Interval then just take the max of end time and merge it
+        if(intervals[i][0]<=currentInterval[1]){
+            currentInterval[1] = max(intervals[i][1],currentInterval[1]);
         }
-        return ans;
+        // non-overlapping means, just put it in answer array and then pick the interval as currentInterval
+        else{
+            ans.push_back(currentInterval);
+            currentInterval = intervals[i];
+        }
+       }
+       ans.push_back(currentInterval);
+       return ans;
     }
 };
