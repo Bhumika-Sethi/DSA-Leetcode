30637646b427a276c601c6c7849c206080a96425
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-01-06 12:37:23 +0530

    Time: 8 ms (7.01%), Space: 12.6 MB (80.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m  = nums2.size();
+        vector<int> nge;
+        vector<int> ans;
         stack<int> st;
-        int n = nums2.size();
-        map<int,int> ans;
-        for(int i = n-1; i>=0; i--){
-            if(st.empty()){
-                ans[nums2[i]] = -1;
-                st.push(nums2[i]);
+        for(int i = m-1; i>=0; i--){
+            while(!st.empty() && nums2[i]> st.top()){
+                st.pop();
             }
-            else if(st.top()<=nums2[i]){
-                while(!st.empty() && st.top()<=nums2[i]){
-                    st.pop();
-                }
-                if(!st.empty()){
-                    ans[nums2[i]] = st.top();
-                    st.push(nums2[i]);                
-                }
-                else{
-                    ans[nums2[i]] = -1;
-                     st.push(nums2[i]);  
-                }
+            if(st.empty()){
+                nge.push_back(-1);
             }
             else{
-                ans[nums2[i]] = st.top();
-                st.push(nums2[i]);                        
+                nge.push_back(st.top());
+            }
+            st.push(nums2[i]);
+        }
+        reverse(nge.begin(),nge.end());
+
+        for(int i = 0; i<n; i++){
+            int toFind = nums1[i];
+            for(int j = 0; j<m; j++){
+                if(toFind==nums2[j]){
+                    ans.push_back(nge[j]);
+                }
             }
         }
-        vector<int> anss;
-        int m = nums1.size();
-        for(int i = 0; i<m; i++){
-            if(ans[nums1[i]]){
-                anss.push_back(ans[nums1[i]]);
-            }
-        }
-        return anss;
+        return ans;
     }
 };
