cefe6ac33c76ee159c339ddd00dfc12293d0747c
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-08-27 15:18:59 +0530

    Time: 4 ms (52.36%), Space: 15 MB (26.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // BETTER/BEST APPROACH -> TC : O(N)
-        //             -> SC : O(N)
-        // In map we'll be storing the element and the index
-        // First check if element in map, if yes return both the indexes
-        // otherwise add the element into the map with its index
+        unordered_map<int,int> mpp;
         int n = nums.size();
-        unordered_map<int,int> mp;
-        vector<int> ans;
+
         for(int i = 0; i<n; i++){
-            int targetEle = target - nums[i];
-           if(mp.find(targetEle)!=mp.end()){
-            return {i,mp[targetEle]};
-           }
-           mp[nums[i]] = i; 
+            if(mpp.find(target-nums[i])!=mpp.end()){
+                return {i,mpp[target-nums[i]]};
+            }
+            mpp[nums[i]] = i;
         }
-        return{-1,-1};
+        return {};
     }
 };
