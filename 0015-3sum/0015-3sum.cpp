0eeb6bf85355295f2549476cea1f84ba00ad00a5
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-08-27 14:53:44 +0530

    Time: 79 ms (12.97%), Space: 35 MB (11.36%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // Optimal approach - TC - O(N^2)+O(NlogN)
-        //                  - SC - O(nonDuplicateTriplet)
-        // Sort the array and eliminate the set space complexity and insertions TC
-        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         int n = nums.size();
+        sort(nums.begin(),nums.end());
         for(int i = 0; i<n; i++){
-            // this we will do to avoid searching for same i again and doing duplication
-            // Check for within limit i.e. i<n as well
             while(i!=0 && i<n && nums[i]==nums[i-1]){
                 i++;
             }
-            int start = i+1;
-            int end = n-1;
-            // NOTE : We on't be using start <= end as we don't want to take same element twice
-             while(start<end){
-                if(nums[i] + nums[start] + nums[end]>0){
-                    end--;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k]<0){
+                    j++;
                 }
-                else if(nums[i] + nums[start] + nums[end]<0){
-                    start++;
+                else if(nums[i]+nums[j]+nums[k]>0){
+                    k--;
                 }
-                else if(nums[i] + nums[start] + nums[end]==0){
-                    vector<int> temp;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[start]);
-                    temp.push_back(nums[end]);
-                    ans.push_back(temp);
-                    start++;
-                    end--;
-                    // Please ensure checks that they don't cross boundary
-                    // Always not that whenever we are incrementing and then doing task, then check for the boundary first
-                    while(start<end && nums[start-1]==nums[start]){
-                        start++;
+                else if(nums[i]+nums[j]+nums[k]==0){
+                    vector<int> a;
+                    a.push_back(nums[i]);
+                    a.push_back(nums[j]);
+                    a.push_back(nums[k]);
+                    ans.push_back(a);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
                     }
-                    while(start<end && nums[end+1]==nums[end]){
-                        end--;
+                    while(j<k && nums[k]==nums[k+1]){
+                        k--;
                     }
                 }
-             }
-             
+            }
         }
         return ans;
     }
