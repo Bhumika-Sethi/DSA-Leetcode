2b76f8dd39aeb0d07368386eb8a88c659098fcd5
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-09-05 19:25:42 +0530

    Time: 677 ms (5.05%), Space: 296.5 MB (13.69%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,39 +1,24 @@
 class Solution {
-public:
-    int LIS(int ind, int prev_ind, int n, vector<int>& nums, vector<vector<int>>& dp){
-        if(ind == n){
+    int lis(int curr_ind, int prev_ind, int n, vector<int>& nums, vector<vector<int>>& dp){
+        if(curr_ind==n){
             return 0;
         }
-        if(dp[ind][prev_ind+1]!=-1){
-            return dp[ind][prev_ind+1];
+        if(dp[curr_ind][prev_ind+1]!=-1){
+            return dp[curr_ind][prev_ind+1];
         }
-
-        // two cases - 
-        // take
         int take = 0;
-        // notTake
-        int notTake = 0;
-        // in case of take the arr[ind]>arr[prev_ind] || if it is the first time we are taking the element
-        if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-            take = 1 + LIS(ind+1,ind,n,nums,dp);
+        if(prev_ind==-1 || nums[prev_ind]<nums[curr_ind]){
+            take = 1 + lis(curr_ind+1,curr_ind,n,nums,dp);
         }
-        notTake = LIS(ind+1,prev_ind,n,nums,dp);
-        // take max of take and notTake
-        return dp[ind][prev_ind+1] = max(take,notTake);
+        int notTake = lis(curr_ind+1,prev_ind,n,nums,dp);
+        return dp[curr_ind][prev_ind+1] = max(take,notTake);
     }
+public:
     int lengthOfLIS(vector<int>& nums) {
+        int curr_ind = 0;
+        int prev_ind = -1;
         int n = nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int ind = n-1; ind>=0; ind--){
-            for(int prev_ind = ind-1; prev_ind>=-1; prev_ind--){
-                int take = 0;
-                if(prev_ind==-1 || nums[ind]>nums[prev_ind]){
-                    take = 1 + dp[ind+1][ind+1];
-                }
-                int notTake = dp[ind+1][prev_ind+1];
-                dp[ind][prev_ind+1] = max(take,notTake);
-            }
-        }
-        return dp[0][0];
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return lis(curr_ind,prev_ind,n,nums,dp);
     }
 };
