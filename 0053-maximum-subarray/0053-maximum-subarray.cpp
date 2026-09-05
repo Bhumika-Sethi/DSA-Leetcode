1c8427ae059c8e662f9c44c93c6fbe08c5922ab5
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-09-05 12:40:53 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,32 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // Optimal approach - kadane algorithm
-        // Printing start and end index as well
-        // return maxSum
-        // TC - O(N)
-        // SC O(1)
+        int curr_sum = nums[0];
+        int max_sum = nums[0];
         int n = nums.size();
-        int sum = 0;
-        int maxi = INT_MIN;
-        int start = -1;
-        int ansStart = -1;
-        int ansEnd = -1;
-        for(int i  = 0; i<n; i++){
-            if(sum==0){
-                start = i;
-            }
-            sum += nums[i];
-            if(maxi<sum){
-                maxi = sum;
-                ansStart = start;
-                ansEnd = i;
-            }
-            if(sum<0){
-                sum = 0;
-            }
+        for(int i = 1; i<n; i++){
+            curr_sum  = max(nums[i],curr_sum+nums[i]);
+            max_sum = max(max_sum,curr_sum);
         }
-        cout<<"Start Index of maximum subarray sum"<<ansStart<<" ending index of maximum index of maximum subarray sub "<<ansEnd<<endl;
-        return maxi;
+        return max_sum;
     }
 };
