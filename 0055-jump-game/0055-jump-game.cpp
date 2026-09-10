3ee115e64fec41072b7da5ddc34d4a0fb133d9d4
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-09-10 13:34:48 +0530

    Time: 557 ms (9.22%), Space: 55.9 MB (5.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool jump(int ind, int n, vector<int>& nums, vector<int>& dp){
-        if(ind==n-1){
+    bool isPossible(int ind, int n, vector<int>& nums, vector<int>& dp){
+        if(ind>=n-1){
             return true;
         }
         if(dp[ind]!=-1){
             return dp[ind];
         }
-        for(int i = 1; i<=nums[ind]; i++){
-            if(jump(ind+i,n,nums,dp)){
-                return dp[ind] = true;
+        for(int jump=1; jump<=nums[ind]; jump++){
+            if(isPossible(ind+jump,n,nums,dp)){
+                return true;
             }
         }
         return dp[ind] = false;
     }
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        return jump(0,n,nums,dp);
+        int i = 0, n = nums.size();
+        vector<int> dp(n+1,-1);
+        return isPossible(i,n,nums,dp);
     }
 };
