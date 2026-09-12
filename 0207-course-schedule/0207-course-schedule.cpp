f3a136a13ae542fbedd20d9ccf7d95e9a8ba3f53
Bhumika-Sethi <67055739+Bhumika-Sethi@users.noreply.github.com>
2026-09-12 13:21:17 +0530

    Time: 0 ms (100%), Space: 18.6 MB (95.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -5,8 +5,8 @@ public:
         queue<int> q;
         vector<int> adj[numCourses];
         for(int i = 0; i<prerequisites.size(); i++){
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-            indegree[prerequisites[i][1]]++;
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
         }
 
         for(int i=0; i<numCourses; i++){
